07236bbdabd7a4541353a69f3a13167ccc930773
Yifaht Meir <yifahtmyers@gmail.com>
2024-02-13 18:12:47 +0200

    Test ball2

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ var gBallSize = 100
 var gBallSizeStep
 
 function onBallClick(maxDiameter) {
-    var elBall = document.querySelector('.ball')
-    //var elBall2 = document.querySelector('.ball2')
+    //var elBall = document.querySelector('.ball')
+    var elBall2 = document.querySelector('.ball2')
     gBallSizeStep = getRandomInt(20, 61)
     if (gBallSize > 400) {
         gBallSize = 100
         elBall.style.transition = 'width 1s, height 1s'
     }
     gBallSize += gBallSizeStep
-    elBall.style.width = gBallSize + 'px'
-    elBall.style.height = gBallSize + 'px'
-    elBall.innerText = gBallSize
-    elBall.style.backgroundColor = getRandomColor()
+    elBall2.style.width = gBallSize + 'px'
+    elBall2.style.height = gBallSize + 'px'
+    elBall2.innerText = gBallSize
+    elBall2.style.backgroundColor = getRandomColor()
 
 } 
