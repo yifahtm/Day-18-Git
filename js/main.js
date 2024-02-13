5291f9c727f858ba7ad0321438322740d47e9477
Yifaht Meir <yifahtmyers@gmail.com>
2024-02-13 15:49:31 +0200

    Add if 400px=100px and 1 second transition

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ var gBallSizeStep = 50
 function onBallClick() {
     var elBall = document.querySelector('.ball')
     gBallSize += 50
+    if (gBallSize > 400) {
+        gBallSize = 100
+        elBall.style.transition = 'width 1s, height 1s'
+    }
     elBall.style.width = gBallSize + 'px'
     elBall.style.height = gBallSize + 'px'
     elBall.innerText = gBallSize
