c1ede6975a63e74f8dcfaadca6c2f9c78cea2a4e
Yifaht Meir <yifahtmyers@gmail.com>
2024-02-13 18:28:00 +0200

    Fix merge conflicts

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ var gBallSizeStep
 
 function onBallClick(maxDiameter) {
     var elBall = document.querySelector('.ball')
-    var elBall = document.querySelector('.ball2')
+    //var elBall2 = document.querySelector('.ball2')
     gBallSizeStep = getRandomInt(20, 61)
     if (gBallSize > 400) {
         gBallSize = 100
-        elBall2.style.transition = 'width 1s, height 1s'
+        elBall.style.transition = 'width 1s, height 1s'
     }
     gBallSize += gBallSizeStep
     elBall.style.width = gBallSize + 'px'
