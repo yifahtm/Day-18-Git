001e068983cced1218ecdacfe999eee7472fc3e4
Yifaht Meir <yifahtmyers@gmail.com>
2024-02-13 18:02:29 +0200

    Change onBallClick() to grow by random num 20- 60 px

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 var gBallSize = 100
-var gBallSizeStep = 50
+var gBallSizeStep
 
 function onBallClick(maxDiameter) {
     var elBall = document.querySelector('.ball')
     var elBall = document.querySelector('.ball2')
-    gBallSize += 50
-    if (gBallSize > maxDiameter) {
+    gBallSizeStep = getRandomInt(20, 61)
+    if (gBallSize > 400) {
         gBallSize = 100
         elBall.style.transition = 'width 1s, height 1s'
     }
+    gBallSize += gBallSizeStep
     elBall.style.width = gBallSize + 'px'
     elBall.style.height = gBallSize + 'px'
     elBall.innerText = gBallSize
