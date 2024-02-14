8481c690b9ce26558c3615629f23a6dd14cd1316
Yifaht Meir <yifahtmyers@gmail.com>
2024-02-14 08:23:08 +0200

    Modify onBallClick&onBall2Click to accommodate operator param and matching ternery

--- js/main.js
+++ js/main.js
@@ -1,30 +1,42 @@
-var gBallSize = 100
-var gBallSizeStep
+var gBall1Size = 100
+var gBall2Size = 100
+var gBall1SizeStep
+var gBall2SizeStep
 
-function onBallClick(maxDiameter) {
+function onBallClick(maxDiameter, operator) {
     var elBall1 = document.querySelector('.ball')
     gBall1SizeStep = getRandomInt(20, 61)
-    if (gBall1Size > 400) {
+    if (gBall1Size > maxDiameter) {
         gBall1Size = 100
         elBall1.style.transition = 'width 1s, height 1s'
     }
-    gBall1Size += gBall1SizeStep
+    gBall2Size = operator === '+' ? gBall2Size + gBall2SizeStep : gBall2Size - gBall2SizeStep
     elBall1.style.width = gBall1Size + 'px'
     elBall1.style.height = gBall1Size + 'px'
     elBall1.innerText = gBall1Size
     elBall1.style.backgroundColor = getRandomColor()
 }
 
-function onBall2Click(maxDiameter) {
+function onBall2Click(maxDiameter, operator) {
     var elBall2 = document.querySelector('.ball2')
     gBall2SizeStep = getRandomInt(20, 61)
-    if (gBall2Size > 400) {
+    if (gBall2Size > maxDiameter) {
         gBall2Size = 100
         elBall2.style.transition = 'width 1s, height 1s'
     }
-    gBallSize += gBallSizeStep
-    elBall2.style.width = gBallSize + 'px'
-    elBall2.style.height = gBallSize + 'px'
-    elBall2.innerText = gBallSize
+    gBall2Size = operator === '+' ? gBall2Size + gBall2SizeStep : gBall2Size - gBall2SizeStep
+    elBall2.style.width = gBall2Size + 'px'
+    elBall2.style.height = gBall2Size + 'px'
+    elBall2.innerText = gBall2Size
     elBall2.style.backgroundColor = getRandomColor()
-} 
+}
+
+function onBall3Click() {
+    var elBall1 = document.querySelector('.ball')
+    var elBall2 = document.querySelector('.ball2')
+    var tempColor = elBall1.style.backgroundColor
+    elBall1.style.backgroundColor = elBall2.style.backgroundColor
+    elBall2.style.backgroundColor = tempColor
+}
+
+
