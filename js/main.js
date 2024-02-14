d8553c9cc546f3b0acc330f6ac113206993d9499
Yifaht Meir <yifahtmyers@gmail.com>
2024-02-14 07:31:38 +0200

    Add onBall2Click func for ball2 similar to the first

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ var gBallSizeStep
 
 function onBallClick(maxDiameter) {
     var elBall = document.querySelector('.ball')
-    //var elBall2 = document.querySelector('.ball2')
     gBallSizeStep = getRandomInt(20, 61)
     if (gBallSize > 400) {
         gBallSize = 100
@@ -14,4 +13,18 @@ function onBallClick(maxDiameter) {
     elBall.style.height = gBallSize + 'px'
     elBall.innerText = gBallSize
     elBall.style.backgroundColor = getRandomColor()
+}
+
+function onBall2Click(maxDiameter) {
+    var elBall2 = document.querySelector('.ball2')
+    gBallSizeStep = getRandomInt(20, 61)
+    if (gBallSize > 400) {
+        gBallSize = 100
+        elBall2.style.transition = 'width 1s, height 1s'
+    }
+    gBallSize += gBallSizeStep
+    elBall2.style.width = gBallSize + 'px'
+    elBall2.style.height = gBallSize + 'px'
+    elBall2.innerText = gBallSize
+    elBall2.style.backgroundColor = getRandomColor()
 } 
