c975ec96651f0ecd82526e2f2f7182d0b89d9670
Yifaht Meir <yifahtmyers@gmail.com>
2024-02-14 13:46:38 +0200

    Remove operator param and ternaries set step for both and set balls sizes and text

--- js/main.js
+++ js/main.js
@@ -3,28 +3,28 @@ var gBall2Size = 100
 var gBall1SizeStep
 var gBall2SizeStep
 
-function onBallClick(maxDiameter, operator) {
+function onBallClick(maxDiameter) {
     var elBall1 = document.querySelector('.ball')
     gBall1SizeStep = getRandomInt(20, 61)
     if (gBall1Size > maxDiameter) {
         gBall1Size = 100
         elBall1.style.transition = 'width 1s, height 1s'
     }
-    gBall2Size = operator === '+' ? gBall2Size + gBall2SizeStep : gBall2Size - gBall2SizeStep
+    gBall1Size += gBall1SizeStep
     elBall1.style.width = gBall1Size + 'px'
     elBall1.style.height = gBall1Size + 'px'
     elBall1.innerText = gBall1Size
     elBall1.style.backgroundColor = getRandomColor()
 }
 
-function onBall2Click(maxDiameter, operator) {
+function onBall2Click(maxDiameter) {
     var elBall2 = document.querySelector('.ball2')
     gBall2SizeStep = getRandomInt(20, 61)
     if (gBall2Size > maxDiameter) {
         gBall2Size = 100
         elBall2.style.transition = 'width 1s, height 1s'
     }
-    gBall2Size = operator === '+' ? gBall2Size + gBall2SizeStep : gBall2Size - gBall2SizeStep
+    gBall2Size += gBall2SizeStep
     elBall2.style.width = gBall2Size + 'px'
     elBall2.style.height = gBall2Size + 'px'
     elBall2.innerText = gBall2Size
@@ -42,10 +42,18 @@ function onBall3Click() {
 function onBall4Click(minDiameter) {
     var elBall1 = document.querySelector('.ball')
     var elBall2 = document.querySelector('.ball2')
-    onBallClick(400, '-')
-    onBall2Click(400, '-')
+    const ballsSizeStep = getRandomInt(20, 61)
     if (gBall2Size < minDiameter || gBall1Size < minDiameter) {
         gBall2Size = minDiameter
         gBall1Size = minDiameter
     }
+    gBall2Size -= ballsSizeStep
+    gBall1Size -= ballsSizeStep
+    elBall2.style.width = gBall2Size + 'px'
+    elBall2.style.height = gBall2Size + 'px'
+    elBall1.style.width = gBall1Size + 'px'
+    elBall1.style.height = gBall1Size + 'px'
+    elBall2.innerText = gBall2Size
+    elBall1.innerText = gBall1Size
+
 }
