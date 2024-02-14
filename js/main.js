01a838be0b2e7829ba93e2ac0d406a10bb0e2c48
Yifaht Meir <yifahtmyers@gmail.com>
2024-02-14 08:08:24 +0200

    Add OnBall3Click func for ball3 to swap colors of 1&2 and make global var for each func

--- js/main.js
+++ js/main.js
@@ -1,30 +1,41 @@
-var gBallSize = 100
-var gBallSizeStep
+var gBall1Size = 100
+var gBall2Size = 100
+var gBall1SizeStep
+var gBall2SizeStep
 
 function onBallClick(maxDiameter) {
-    var elBall = document.querySelector('.ball')
-    gBallSizeStep = getRandomInt(20, 61)
-    if (gBallSize > 400) {
-        gBallSize = 100
-        elBall.style.transition = 'width 1s, height 1s'
+    var elBall1 = document.querySelector('.ball')
+    gBall1SizeStep = getRandomInt(20, 61)
+    if (gBall1Size > 400) {
+        gBall1Size = 100
+        elBall1.style.transition = 'width 1s, height 1s'
     }
-    gBallSize += gBallSizeStep
-    elBall.style.width = gBallSize + 'px'
-    elBall.style.height = gBallSize + 'px'
-    elBall.innerText = gBallSize
-    elBall.style.backgroundColor = getRandomColor()
+    gBall1Size += gBall1SizeStep
+    elBall1.style.width = gBall1Size + 'px'
+    elBall1.style.height = gBall1Size + 'px'
+    elBall1.innerText = gBall1Size
+    elBall1.style.backgroundColor = getRandomColor()
 }
 
 function onBall2Click(maxDiameter) {
     var elBall2 = document.querySelector('.ball2')
-    gBallSizeStep = getRandomInt(20, 61)
-    if (gBallSize > 400) {
-        gBallSize = 100
+    gBall2SizeStep = getRandomInt(20, 61)
+    if (gBall2Size > 400) {
+        gBall2Size = 100
         elBall2.style.transition = 'width 1s, height 1s'
     }
-    gBallSize += gBallSizeStep
-    elBall2.style.width = gBallSize + 'px'
-    elBall2.style.height = gBallSize + 'px'
-    elBall2.innerText = gBallSize
+    gBall2Size += gBall2SizeStep
+    elBall2.style.width = gBall2Size + 'px'
+    elBall2.style.height = gBall2Size + 'px'
+    elBall2.innerText = gBall2Size
     elBall2.style.backgroundColor = getRandomColor()
-} 
+}
+
+function onBall3Click() {
+    var elBall1 = document.querySelector('.ball')
+    var elBall2 = document.querySelector('.ball2')
+    var tempColor = elBall1.style.backgroundColor
+    elBall1.style.backgroundColor = elBall2.style.backgroundColor
+    elBall2.style.backgroundColor = tempColor
+}
+
